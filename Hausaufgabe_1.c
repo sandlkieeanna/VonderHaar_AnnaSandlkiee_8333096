b39b11d023a5a55d02696e8c065e0755fc3beba9
sandlkieeanna <120455192+sandlkieeanna@users.noreply.github.com>
2022-12-20 10:28:19 +0100

    Update Hausaufgabe_1.c

--- Hausaufgabe_1.c
+++ Hausaufgabe_1.c
@@ -1,42 +1,90 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "tr.h"
 
-struct complex
-{
-    double a;
-    double b;
-} num1, num2;
-void add (struct complex num1, struct complex num2);
-main(){
-    char opt;
-
-    printf("Please enter your first complex number: \n");
-    scanf("%d%d", &num1.a, &num1.b);
-
-    printf("Please choose one of the following operator: + / - * \n");
-    scanf("%c", &opt);
-
-    printf("Please enter your second complex number:\n");
-    scanf("%d%d", &num2.a, &num2.b);
-
-    add(num1, num2);
+struct complex{
+    double real;
+    double img;
 }
-void add( struct complex num1, struct complex num2){
-struct complex ans;
-    switch (opt)
-    {
+ main(){
+    struct complex num1,num2,result;
+    char opt;
+    // double num1;
+    // double num2;
+    // double result;
+
+    printf("Please enter an operator: \n");
+    scanf ("%c",&opt);
+
+    printf("Please enter your first number: \n");
+    scanf("%lf%lf",&num1.real,&num1.img);
+
+    printf("Please enter your second number: \n");
+    scanf("%lf%lf",&num2.real,&num2.img);
+    double wonder();
+    switch (opt){
     case '+':
-        ans.a= num1.a+num1.a;
-        ans.b= num2.b+ num2.b;
-        printf(" The sum is:\n %d+i%d",&ans.a,&ans.b);
+        result.real= num1.real+num2.real;
+        result.img= num1.img+num2.img;
+        printf("The result: %lf+i%lf", result.real,result.img);
+        break;
+    case '-':
+        result.real= num1.real-num2.real;
+        result.img= num1.img-num2.img;
+        printf("The result: %lf+i%lf", result.real,result.img);
+        break;
+    case '*':
+        result.real= (num1.real*num2.real-num1.img*num2.img);
+        result.img= (num1.real*num2.img+num1.img*num2.real);
+        printf("The result: %lf+i%lf", result.real,result.img);
+        break;
+    case '/':
+        result.real= (num1.real*num2.real+num1.img*num2.img)/(num2.real*num2.real+num2.img*num2.img);
+        result.img= (num1.img/num2.real-num1.real*num2.img)/(num2.real*num2.real+num2.img*num2.img);
+        printf("The result: %lf+i%lf", result.real,result.img);
         break;
     
-    case '-':
-        ans.a= num1.a-num1.a;
-        ans.b= num2.b-num2.b;
-        printf(" The diffenence is:\n %d+i%d",&ans.a,&ans.b);
+    default:
+        printf("please enter a valid operator, thx!");
         break;
+    }
 
-    case '*':
+    char format;
+    printf("If you want to change the format, please enter one (k,p,e):");
+    scanf("%c",&format);
+    double radius;
+    radius=sqrt(result.real*result.real+result.img);
+    double w;
+    w= 1/ tan(result.img/result.real);
+    double theo;
+    theo=cos(w);
+    double leo;
+    leo= sin(w);
+    double mia;
+    mia= cos(w);
+    double nina;
+    nina= sin(w);
+    double maya= mia+nina;
+    double exp(double maya);
+
+    switch (format)
+    {
+     case 'k':
+        printf("The result is:\n %d%d",&result.real,&result.img);        
+        break;
+    case 'p':
+        printf("The result is: \n %d*(%d+i%d)",&radius,&theo,&leo);
+        break;
+    case 'e':
+        printf("The result is: \n %d*%d",&radius,&exp);
+        break;
+    default:
+        printf("Thank you, have a nice day!");
+        break;
+    }
+
+}
         ans.a=(num1.a*num2.a-num2.a*num2.b);
         ans.b=(num1.a*num2.b+num2.a*num1.b);
         printf("The product is: \n %d+i%d", &ans.a, &ans.b);
